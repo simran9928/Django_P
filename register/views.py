d4b59791160d810a31b5b160722b73b52f54b3ac
simran9928 <simranofficial99@gmail.com>
2021-06-02 19:39:32 +0530

    LOGIN

---
+++ register/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegistrationForm
+from django.contrib.auth.decorators import login_required
+
+def register(request):
+    if request.method== 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username= form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login')
+    else:
+        form=UserRegistrationForm()
+    return render(request, 'register.html', {'form' : form})
+
+@login_required
+def profile(request):
+    return render(request, 'profile.html')
